ea9bdb7d4ed001ab07c6b5d3a09a6cab34c4aac1
John Yong <lhlhyong@gmail.com>
2018-05-04 16:52:53 -0700

    add destination endpoint

--- index.js
+++ index.js
@@ -12,3 +12,18 @@ app.listen(process.env.PORT, function() {
 app.get('/', function(req, res) {
     res.send("Hello, World!");
 });
+
+app.get('/destination', function(req, res) {
+	if (req.headers['verification-token'] === process.env.DESTINATION_VERIFICATION_TOKEN) {
+		console.log('verification-token matched!');
+		return res.send(req.query.challenge);
+	}
+
+	console.log('verfication-token did not match :( ');
+	res.sendStatus(400);
+});
+
+app.post('/destination', function(req, res) {
+    console.log(req.body);
+    res.sendStatus(200);
+})
